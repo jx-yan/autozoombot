e6deb0e09fcbb4a7d8acbb8b8f7960c7c091f207
Kaz1787 <jiaxing.yan.2020@sis.smu.edu.sg>
2021-08-07 03:53:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 const {Telegraf, Markup} = require('telegraf');
-const fetch = require('node-fetch').default
-const {MenuTemplate, MenuMiddleware} = require('telegraf-inline-menu')
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 if (bot === undefined) {
@@ -20,10 +18,128 @@ bot.start((ctx) => {
                 [
                     {text: 'Ian', callback_data:'ian'},
                     {text: 'NatChua',callback_data:'nat'}
+                ],
+                [
+                    {text: 'Exit', callback_data: 'exit'}
                 ]
             ]
         }
-    })    
+    })
 })
+
+bot.action('exit', ctx => {
+    ctx.answerCbQuery();
+    ctx.reply('Thank you for using AutoZoom!')
+})
+
+bot.action('jx', ctx => {
+    // ctx.answerCbQuery('Timetable loaded');
+    // ctx.reply('Tue\n0815 - 1130: IS210 - BPAS (G6)\n1530 - 1845: IS216 - WAD2 (G6)\n\nThu\n0815 - 1130: IS211 - IDP (G9)\n\nFri\n1200 - 1515: CORIS1702 - CT (G5)')
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, 'Tue\n0815 - 1130: IS210 - BPAS (G6)\n1530 - 1845: IS216 - WAD2 (G6)\n\nThu\n0815 - 1130: IS211 - IDP (G9)\n\nFri\n1200 - 1515: CORIS1702 - CT (G5)',
+    {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text:'BPAS Zoom', callback_data:'bpaszoom'},
+                    {text:'WAD Zoom', callback_data:'wadzoom'}
+                ],
+                [
+                    {text:'IDP Zoom', callback_data:'idpzoom'},
+                    {text:'CT Zoom', callback_data:'ctzoom'}
+                ],
+                [
+                    {text:'Back to Menu', callback_data:'menu'}
+                ]
+            ]
+        }
+    })
+})
+
+bot.action('ian', ctx => {
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, 'Tue\n0815 - 1130: IS210 - BPAS (G6)\n1530 - 1845: IS216 - WAD2 (G6)\n\nThu\n0815 - 1130: IS211 - IDP (G9)\n\nFri\n1200 - 1515: CORIS1702 - CT (G5)',
+    {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text:'BPAS Zoom', callback_data:'bpaszoom'},
+                    {text:'WAD Zoom', callback_data:'wadzoom'}
+                ],
+                [
+                    {text:'IDP Zoom', callback_data:'idpzoom'},
+                    {text:'CT Zoom', callback_data:'ctzoom'}
+                ],
+                [
+                    {text:'Back to Menu', callback_data:'menu'}
+                ]
+            ]
+        }
+    })
+})
+
+bot.action('nat', ctx => {
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, 'Tue\n0815 - 1130: IS210 - BPAS (G6)\n1530 - 1845: IS216 - WAD2 (G6)\n\nThu\n0815 - 1130: IS211 - IDP (G9)\n\nFri\n1200 - 1515: CORIS1702 - CT (G5)',
+    {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text:'BPAS Zoom', callback_data:'bpaszoom'},
+                    {text:'WAD Zoom', callback_data:'wadzoom'}
+                ],
+                [
+                    {text:'IDP Zoom', callback_data:'idpzoom'},
+                    {text:'CT Zoom', callback_data:'ctzoom'}
+                ],
+                [
+                    {text:'Back to Menu', callback_data:'menu'}
+                ]
+            ]
+        }
+    })
+})
+
+bot.action('val', ctx => {
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, 'Tue\n0815 - 1130: IS210 - BPAS (G6)\n1530 - 1845: IS216 - WAD2 (G6)\n\nThu\n0815 - 1130: IS211 - IDP (G9)\n\nFri\n1200 - 1515: CORIS1702 - CT (G5)',
+    {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text:'BPAS Zoom', callback_data:'bpaszoom'},
+                    {text:'WAD Zoom', callback_data:'wadzoom'}
+                ],
+                [
+                    {text:'IDP Zoom', callback_data:'idpzoom'},
+                    {text:'CT Zoom', callback_data:'ctzoom'}
+                ],
+                [
+                    {text:'Back to Menu', callback_data:'menu'}
+                ]
+            ]
+        }
+    })
+})
+
+bot.action('menu',ctx => {
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id,'Click to check your timetable',
+    {
+        reply_markup: {
+            inline_keyboard:[   
+                [
+                    {text: 'JX', callback_data:'jx'},
+                    {text: 'Valencia',callback_data:'val'}
+                ],
+                [
+                    {text: 'Ian', callback_data:'ian'},
+                    {text: 'NatChua',callback_data:'nat'}
+                ]
+            ]
+        }
+    })
+})
+
 
 bot.launch();
